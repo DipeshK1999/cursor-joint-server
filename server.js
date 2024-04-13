feda36e8284d70c39daf73d42369df857c106f15
DipeshK1999 <148889685+DipeshK1999@users.noreply.github.com>
2024-04-14 00:56:57 +0200

    config

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ const io = require("socket.io")(server, {
   cors: {
     origin: "*",
     methods: ["GET", "POST"],
+    transports: ["websocket", "polling"],
   },
+  allowEIO3: true,
 });
 
 app.get("/", (req, res) => {
